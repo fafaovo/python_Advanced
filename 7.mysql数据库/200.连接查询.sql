13ca6b7b1a9c21ff48347cb48f0a42abe48afbdf
fafaovo <aa1206180803@sina.cn>
2021-05-07 22:54:47 +0800

    2021-5-7

--- 7.mysql数据库/200.连接查询.sql
+++ 7.mysql数据库/200.连接查询.sql
@@ -1,7 +1,7 @@
 /*
 内连接查询 查询两个表匹配的数据    ([)(])
 右(外)连接查询 查询结果为两个表匹配的数据和右表特有的数据  ([)()]
-左(内)连接查询 查询结果为两个表匹配的数据和左表特有的数据  [()(])
+左(外)连接查询 查询结果为两个表匹配的数据和左表特有的数据  [()(])
 内外连接查询对于另外一张表中不存在的数据使用null填充
 */
 
@@ -16,3 +16,14 @@ select * from students,classes where students.cls_id = classes.id;
 select stu.name '姓名',cla.name '班级' from 
 students stu,classes cla WHERE 
 stu.cls_id =  cla.id;
+
+
+--在inner join 中可以使用on来代替where
+select stu.name '姓名',cla.name '班级' from 
+students stu inner join classes cla ON 
+stu.cls_id =  cla.id;
+
+--按照班级的编号进行排序
+select cla.name,stu.* from 
+students as stu inner join classes as cla ON 
+stu.cls_id =  cla.id order by cla.id asc ,stu.id asc;
---
+++ 7.mysql数据库/201.左右链接.sql
@@ -0,0 +1,13 @@
+/*
+左连接:主表 left join 从表 on 连接条件
+右链接:从表 right join 主表 on 连接条件
+
+保留主表所有的结果，从表没连接成功则补null
+*/
+--查询每位学生对应的班级信息
+select * from students left join classes on students.cls_id = classes.id;
+
+--查询没有对应班级的学生
+select students.* from students left join classes  
+on students.cls_id = classes.id where classes.id is null;
+
